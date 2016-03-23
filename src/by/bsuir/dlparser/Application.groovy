152978a1133e3ebf653a0feb042b5f8d59d68835
savchik <p.savchik@gmail.com>
2016-03-23 19:57:49 +0300

    Implemented basic application cascade.

---
+++ src/by/bsuir/dlparser/Application.groovy
@@ -0,0 +1,20 @@
+package by.bsuir.dlparser
+import groovy.swing.SwingBuilder
+import javax.swing.JFrame
+
+class Application {
+    static initWindow(){
+        def form = new ApplicationForm()
+        def frame = new SwingBuilder().frame(
+                title:"DLParser",
+                contentPane: form.mainPanel,
+                defaultCloseOperation: JFrame.EXIT_ON_CLOSE
+        )
+        frame.pack()
+        frame.setVisible(true)
+    }
+
+    static void main(String[] args){
+        initWindow()
+    }
+}
---
+++ src/by/bsuir/dlparser/ApplicationForm.java
@@ -0,0 +1,11 @@
+package by.bsuir.dlparser;
+
+import javax.swing.*;
+
+/**
+ * Created by savchik on 3/23/2016.
+ */
+public class ApplicationForm {
+    private JPanel panel1;
+    private JPanel mainPanel;
+}
